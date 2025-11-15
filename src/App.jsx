06b3273521998b3f7505ee23ccd22d6856976ae3
Satyam Jha <satyamjha2743@gmail.com>
2025-11-15 17:43:47 +0530

    Setup: setup frontend main components and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import { MyContext } from "./MyContext.jsx";
+import ChatWindow from "./ChatWindow.jsx";
+import Sidebar from "./Sidebar.jsx";
 
 function App() {
-  return <></>;
+  const ProviderValues = {};
+  return (
+    <div className="app">
+      <MyContext.Provider value={ProviderValues}>
+        <Sidebar></Sidebar>
+        <ChatWindow></ChatWindow>
+      </MyContext.Provider>
+    </div>
+  );
 }
 
 export default App;

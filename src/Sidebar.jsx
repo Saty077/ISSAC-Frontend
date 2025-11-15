5a7c49f5a72c4fc45299d3d8f77ca9d6b6f76d81
Satyam Jha <satyamjha2743@gmil.com>
2025-11-15 19:59:46 +0530

    feat: add style to Sidebar component

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,7 +1,23 @@
 import "./Sidebar.css";
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <section className="Sidebar">
+      <button>
+        <img className="logo" src="\src\assets\Isac3.jpeg" alt="issac_logo" />
+        <div>ISSAC-AI</div>
+        <span>
+          <i className="fa-solid fa-pen-to-square"></i>
+        </span>
+      </button>
+      <ul className="history">
+        <li>Thread1</li>
+        <li>Thread2</li>
+        <li>Thread3</li>
+      </ul>
+      <div className="sign">By Satyam &hearts;</div>
+    </section>
+  );
 }
 
 export default Sidebar;

54c692d75aef36b0e6f7aef0e378b8508936b7c6
Satyam Jha <satyamjha2743@gmil.com>
2025-11-16 11:40:51 +0530

    feat: build and style the ChatWindow component

--- src/ChatWindow.jsx
+++ src/ChatWindow.jsx
@@ -1,7 +1,31 @@
 import "./ChatWindow.css";
+import Chat from "./Chat.jsx";
 
 function ChatWindow() {
-  return <div>ChatWindow</div>;
+  return (
+    <div className="chatwindow">
+      <div className="navbar">
+        <span>
+          ISSAC-AI <i class="fa-solid fa-angle-down"></i>
+        </span>
+        <div className="userIconDiv">
+          <span className="userIcon">
+            <i class="fa-solid fa-user"></i>
+          </span>
+        </div>
+      </div>
+      <Chat></Chat>
+      <div className="chatInput">
+        <div className="inputBox">
+          <input type="text" placeholder="Ask anything" />
+          <div className="submit">
+            <i class="fas fa-paper-plane"></i>
+          </div>
+        </div>
+        <p className="info">ISSAC-AI can make mistakes</p>
+      </div>
+    </div>
+  );
 }
 
 export default ChatWindow;

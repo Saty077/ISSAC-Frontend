f4cad71ba331f9689a09c13770fb0acfb0ba52b9
Satyam Jha <satyamjha2743@gmail.com>
2025-11-21 17:43:35 +0530

    feat: Configure New Chat in Sidebar Component

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,9 +1,19 @@
 import { useContext, useEffect, useState } from "react";
 import { MyContext } from "./MyContext";
 import "./Sidebar.css";
+import { v1 as uuidv1 } from "uuid";
 
 function Sidebar() {
-  const { allThread, setAllThread, currThreadId } = useContext(MyContext);
+  const {
+    allThread,
+    setAllThread,
+    currThreadId,
+    setPrompt,
+    setReply,
+    setCurrThreadId,
+    setNewChat,
+    setPrevChats,
+  } = useContext(MyContext);
 
   const getAllThreads = async () => {
     const response = await fetch("http://localhost:8080/api/thread");
@@ -17,17 +27,39 @@ function Sidebar() {
 
   useEffect(() => {
     getAllThreads();
-  }, []);
+  }, [currThreadId]);
+
+  const createNewChat = async () => {
+    setNewChat(true);
+    setPrompt("");
+    setReply(null);
+    setCurrThreadId(uuidv1());
+    setPrevChats([]);
+  };
 
   return (
     <section className="Sidebar">
-      <button>
-        <img className="logo" src="\src\assets\Isac3.jpeg" alt="issac_logo" />
-        <div>ISSAC-AI</div>
-        <span>
-          <i className="fa-solid fa-pen-to-square"></i>
-        </span>
-      </button>
+      <div className="upperSection">
+        <div className="logoClass">
+          <div>
+            <img
+              className="logo"
+              src="\src\assets\Isac3.jpeg"
+              alt="issac_logo"
+            />
+          </div>
+          <div>ISSAC-AI</div>
+        </div>
+        <button onClick={createNewChat}>
+          <div>
+            <p>New Chat</p>
+          </div>
+          <span>
+            <i className="fa-solid fa-pen-to-square"></i>
+          </span>
+        </button>
+      </div>
+
       <ul className="history">
         {allThread?.map((thread, idx) => (
           <li key={idx}>{thread.title}</li>
